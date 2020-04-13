165fc71bd60a51badf25d93cc280140fc4d5ba2e
Christina Harris <christinathedesigner@outlook.com>
2020-04-13 12:37:32 -0700

    initial commit

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import {connect} from "react-redux"
+import { removeFeature } from "../Actions"
 
 const AddedFeature = props => {
+
+  const handleRemove = () => {
+    props.removeFeature(props.feature)
+  }
+
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button onClick={handleRemove} className="button">X</button>
       {props.feature.name}
     </li>
   );
 };
 
-export default AddedFeature;
+export default connect(null, {removeFeature})(AddedFeature);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import { createStore } from 'redux';
 import 'bulma/css/bulma.css';
 import './styles.scss';
+import { reducer } from "./reducers"
+import { Provider } from 'react-redux';
+
+const store = createStore(reducer);
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Provider store={store}>
+    <App/>
+  </Provider>, rootElement);
+
+
+//import createStore - allows us to create a Redux state store and it needs to take a function that needs to manipulate the state
+// import reducer is the function that manipulate our state that we pass to create store
+//import provider is the wrapper which allows state to be used by everything inside of our app
